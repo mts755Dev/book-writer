ccb185d0fdf0495ffdf55c5b3d9c12bc0e873098
mts755Dev <talha.saleem@devsinc.com>
2024-01-19 05:52:54 +0500

    app styling change

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Cloud Book Store</div>;
+  return (
+    <div className="flex items-center text-black justify-center pt-40 text-center">
+      <div>
+        <h1 className="text-3xl">Welcome</h1>
+        <h1 className="text-3xl">to</h1>
+        <h1 className="text-3xl font-bold">Cloud Book Writer Platform</h1>
+      </div>
+    </div>
+  );
 }
